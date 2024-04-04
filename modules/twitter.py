e99b8e3b9401443a43645f259a75c989ee184ea4
spiiritual <venommortalx@gmail.com>
2024-04-04 08:48:05 -0400

    add retweet function

--- modules/twitter.py
+++ modules/twitter.py
@@ -53,3 +53,9 @@ def get_hashtags_for_question_type(type : str) -> str:
 
 load_dotenv()
 
+def retweet(tweet_id : str):
+    client = Client(language="en-US")
+    client.load_cookies("cookies.json")
+
+    client.retweet(tweet_id)
+
